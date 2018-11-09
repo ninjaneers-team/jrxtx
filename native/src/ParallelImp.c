1ea6a41d7069bb68fb4e9d68a31ea6a2445f1448
Claus Strasburger <claus@strasburger.de>
2018-11-09 14:48:08 +0100

    Apply patches found in freebsd port of rxtx
    Makefile
    1) Missing dependancy on plibthread.so.1
    
    Java code base
    2) Added commandline debug output in multiple Java files
    
    3) Tided comment statements, tabulation and white space to aid reading
    of sections of the original source code
    
    4) Added additional debug statements to aid diagnosis in multiple Java
    files
    
    5) Moved error logging from Zystem to be local in the RXTXPort.java
    file (was never firing from Zystem); now common with rest of codebase
    
    6) Ensured that RXTX only outputs debug level information when
    requested.
    
    7) Only enumerate the local ports on initialisation once (use of
    init_clean), rather than three times.
    
    8) Updated ports lists to reflect current usage pre FreeBSD v10 and post
    v10.
    
    9) Debug identifier inserted before all calls
    
    10) correctly read the properties file when there are multiple java
    library paths
    
    11) Enabled the skipping of parrellel ports during scans where they are
    not used by the invoking java parent base to RXTX (Still allows them to
    be manually specified in gn.io.parellel env arg)
    
    SerialImp.c
    12) Tidied code indentation (where needed, it is a medium length file
    and doesn't provide benifit on areas that work)
    
    13) Added standard out debug support to allow for assessment of
    interaction with Java code that can be compiled in (DEBUG_STDOUT)
    
    14) Added entry and exist debug statement for key functions
    
    15) where sprintf is being used, ensure that the existing ptr refers to
    an empty string (uses memset to sizeof)
    
    16) cleared up display tabulation of function headers in entire file (on
    second enumeration strings contained garbage)
    
    17) Remove LEAVE and ENTRY functions as not working as expected in
    header.
    
    18) Made sure all debug statements are null pointer terminated (note
    sprintf is not solely used for debug message formatting)
    
    19) Added condition so that a port that is in use during testread() is
    still reported as selectable. (If you attempt to open it after the port
    scan and it is still EBUSY the behaviour is unaltered and will throw a
    suitable error back)
    
    20) Disabled uucp for lpt ports, as uucp only applies to tty on
    FreeBSD (so says the Handbook...)
    
    21) RXTXPort:interruptEventLoop - resolved SIGABRT call to
    process (intended to kill off thread not entire JRE)...
    
    22) RXTXPort:interruptEventLoop now waits for timer in child thread to
    expire before attempting to closing the thread (delay between 0 and
    1000000 sleeps)
    
    23) All debug streams for FreeBSD now forward to same function for
    uniform formatting.
    
    This fixes two downstream issues in the arduino port, which results in
    missing dependancies, a core dump when uploading, or very slow port
    enumeration (up to 30s in some cases) when the arduino IDE is first
    started.
    
    PR:                  200179
    Submitted by:        james@elstone.net
    Approved by:         maintainer timeout (mirror176@cox.net ; 45 days)

--- native/src/ParallelImp.c
+++ native/src/ParallelImp.c
@@ -265,9 +265,6 @@ JNIEXPORT jboolean JNICALL LPRPort(isPrinterBusy)(JNIEnv *env,
 #if defined(__linux__)
 	return( status & LP_BUSY ? JNI_TRUE : JNI_FALSE );
 #endif
-#if defined(__FreeBSD__)
-	return( status & EBUSY ? JNI_TRUE : JNI_FALSE );
-#endif
 	return(JNI_FALSE);
 }
 /*----------------------------------------------------------
